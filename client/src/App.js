cd6d1663e6ff4e64424e1a315ee8e69437c388a3
jarray-kais <jarraykais1@gmail.com>
2024-02-25 22:02:57 +0100

    productScreen
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import data from "./data";
+import { BrowserRouter, Route , Routes } from 'react-router-dom';
+import ProductScreen from "./screens/ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -15,39 +18,14 @@ function App() {
         </div>
       </header>
       <main>
-      
-        <div className="row center">
-
-        {
-          data.products.map((product)=>(
-            <div className="card" key={product._id}>
-            <a href={`/product/${product._id}`}
-              ><img src={product.image} alt={product.name}
-            /></a>
-            <div className="card-body">
-              <a href={`/product/${product._id}`}>
-                <h2>{product.name}</h2>
-              </a>
-
-              <div className="rating">
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star half-o"></i></span>
-              </div>
-              <div className="price">${product.price}</div>
-            </div>
-          </div>
-          ))
-        }
-       
-          
-          
-        </div>
+      <Routes> 
+      <Route path="/product/:id" element={<ProductScreen/>}></Route>
+       <Route path="/" element={<HomeScreen/>} exact></Route> 
+      </Routes>
       </main>
       <footer className="row center">ALL RIGHT RESERVED</footer>
     </div>
+    </BrowserRouter>
   );
 }
 
