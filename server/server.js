5421bc9a4994767aa376e6aee1881d0b22f05a9d
jarray-kais <jarraykais1@gmail.com>
2024-02-27 00:49:11 +0100

    Create-Sample-Products-In-MongoDB
    Signed-off-by: jarray-kais <jarraykais1@gmail.com>

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv'
 
 
 import userRouter from './Routes/userRoutes.js';
+import productRouter from './Routes/productRouter.js';
 
 dotenv.config();
 
@@ -20,22 +21,12 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazona-redux')
 const app = express();
 app.use(express.json(), express.urlencoded({ extended: true }),cors());
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x)=>x._id=== req.params.id)
-  if(product){
-    res.send(product)
-  }
-  else{
-    res.status(404).send({message : 'Product Not Found'})
-  }
-});
-
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
-
 
 app.use('/api/users', userRouter);
+app.use('/api/products',productRouter)
+
+
+
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
